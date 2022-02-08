fc063c621a9a7ad031518e751c56049054f13df9
kangyecn <93654227+kangyecn@users.noreply.github.com>
2022-02-09 10:59:14 +1100

    Time: 43 ms (57.15%), Space: 14 MB (90.62%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -1,35 +1,28 @@
 class Solution(object):
     def myAtoi(self, s):
-        # Remove any additional spaces before the given string
         s = s.lstrip()
         
-        # If string is empty return 0
         if len(s) == 0:
             return 0
 
-        # String index from where the processing should start
         start = 0
         
-        # Handling numbers with +/- sign
         sign_multiplier = 1
-        if s[0] == '-': # Handling for negative sign numbers
+        if s[0] == '-':
             sign_multiplier = -1
             start = 1
-        elif s[0] == '+': # Handling for signed positive sign number
+        elif s[0] == '+': 
             start = 1
 
         result = 0
 
         index = start
         while index < len(s):
-            # Handling for non numeric values
             if not('0' <= s[index] <= '9'):
                 return result * sign_multiplier
 
-            # Calculate the result for current iteration
             result = result * 10 + ord(s[index]) - ord('0')
 
-            # Check if result exceeds MinInt32 or MaxInt32
             min_int_32 = 2 ** 31
             if result * sign_multiplier <= -min_int_32:
                 return -min_int_32
