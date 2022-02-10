cd61cc729164e2107750526ea92d317796893915
kangyecn <93654227+kangyecn@users.noreply.github.com>
2022-02-10 16:00:34 +1100

    Time: 123 ms (40.47%), Space: 15.4 MB (55.84%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,19 +1,21 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        left_most_element = self.find_extremity(nums, target, "Left Extremity")
-        right_most_element = self.find_extremity(nums, target, "Right Extremity")
-        return [left_most_element, right_most_element]
-        
-    def find_extremity(self, nums, target, key):
-        left_pointer, right_pointer, temp = 0, len(nums) - 1, -1
-        while left_pointer <= right_pointer:
-            middle = (left_pointer + right_pointer) // 2
-            if nums[middle] < target: left_pointer = middle + 1
-            elif target < nums[middle]: right_pointer = middle -1
-            else:
-                temp = middle
-                if key == "Left Extremity": right_pointer = middle -1
-                else: left_pointer = middle + 1
-                    
-        return temp
+        start, end = 0, len(nums) - 1
+        lo = hi = -1
+    
+        while (lo == -1 or hi == -1) and len(nums) > 0:
+            if nums[start] != target and lo == -1:
+                start += 1
+            if nums[end] != target and hi == -1:
+                end -= 1
+
+            if start > end:
+                break
+
+            if nums[start] == target:
+                lo = start
+
+            if nums[end] == target:
+                hi = end
+    
+        return [lo, hi]
