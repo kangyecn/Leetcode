e985c22b6d04f1b436ce850abf56847044c39d14
kangyecn <93654227+kangyecn@users.noreply.github.com>
2022-02-10 15:42:30 +1100

    Time: 40 ms (56.95%), Space: 13.9 MB (96.33%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.py
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -6,7 +6,7 @@ class Solution:
         divisor = abs(divisor)
         for i in range(31,-1,-1):
             if divisor * (1 << i) <= dividend:
-                ans = ans | (1 << i)
+                ans = ans + (1 << i)
                 dividend = dividend - (divisor * (1 << i))
         if sign == True:
             ans = -1 * ans
