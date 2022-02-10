0897066d9baf2d7c17da6e48e12a944a0e5b0ea9
kangyecn <93654227+kangyecn@users.noreply.github.com>
2022-02-11 08:47:58 +1100

    Time: 112 ms (60.24%), Space: 13.8 MB (96.85%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,22 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
 	
-        def is_valid(board):
-            for row in board:
-                s = Counter(row)
-                s["."] = 0
-                if s.most_common(1)[0][1] > 1:
-                    return False
-            return True
-		
-        return is_valid(board) and \
-            is_valid(zip(*board)) and \
-            is_valid(
-            [
-                board[  x * 3  ][y * 3: y * 3 + 3] +
-                board[x * 3 + 1][y * 3: y * 3 + 3] +
-                board[x * 3 + 2][y * 3: y * 3 + 3]
-                for x in range(3)
-                for y in range(3)
-            ]
-        )        
+        for i in range(9):
+            col = []
+            row = []
+            block = []
+            for j in range(9):
+                c = board[i][j] # Goes columnwise
+                if c != '.':
+                    col.append(c)
+                r = board[j][i] # Goes rowwise
+                if r != '.':
+                    row.append(r)
+                b = board[(i//3)*3+j//3][(i%3)*3+j%3] # Goes blockwise
+                if b != '.':
+                    block.append(b)
+            if len(col) != len(set(col)) or len(row) != len(set(row)) or len(block) != len(set(block)):
+                return False
+        return True   
