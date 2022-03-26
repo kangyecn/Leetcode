38454e9aa43e633a1cbd7ba0f6a8a0f5f4620125
kangyecn <93654227+kangyecn@users.noreply.github.com>
2022-03-26 22:14:35 +1100

    Time: 355 ms (48.05%), Space: 0B (100.00%) - LeetHub

--- 601-human-traffic-of-stadium/601-human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/601-human-traffic-of-stadium.sql
@@ -1,10 +1,10 @@
-select distinct s1.* from Stadium s1, stadium s2, stadium s3 where
-s1.people >= 100 and s2.people >= 100 and s3.people >= 100 and 
-(
+select distinct s1.* from stadium s1, stadium s2, stadium s3
+where s1.people >= 100 and s2.people >= 100 and s3.people >=100
+and (
     (s1.id=s2.id-1 and s1.id=s3.id-2)
     or
     (s1.id=s3.id+1 and s1.id=s2.id-1)
     or
     (s1.id=s2.id+2 and s1.id=s3.id+1)
-    )
-order by s1.id;
+)
+order by s1.id
