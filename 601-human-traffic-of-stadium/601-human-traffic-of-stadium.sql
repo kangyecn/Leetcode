15cd6766e7456ed9847fd7b1b9fc4731d701f513
kangyecn <93654227+kangyecn@users.noreply.github.com>
2022-03-26 22:11:39 +1100

    Time: 355 ms (48.05%), Space: 0B (100.00%) - LeetHub

--- 601-human-traffic-of-stadium/601-human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/601-human-traffic-of-stadium.sql
@@ -1,13 +1,10 @@
-# Write your MySQL query statement below
-select distinct t1.*
-from stadium t1, stadium t2, stadium t3
-where t1.people >= 100 and t2.people >= 100 and t3.people >= 100
-and
+select distinct s1.* from Stadium s1, stadium s2, stadium s3 where
+s1.people >= 100 and s2.people >= 100 and s3.people >= 100 and 
 (
-    (t1.id - t2.id = 1 and t1.id - t3.id = 2 and t2.id - t3.id =1)  -- t1, t2, t3
+    (s1.id=s2.id-1 and s1.id=s3.id-2)
     or
-    (t2.id - t1.id = 1 and t2.id - t3.id = 2 and t1.id - t3.id =1) -- t2, t1, t3
+    (s1.id=s3.id+1 and s1.id=s2.id-1)
     or
-    (t3.id - t2.id = 1 and t2.id - t1.id =1 and t3.id - t1.id = 2) -- t3, t2, t1
-)
-order by t1.id
+    (s1.id=s2.id+2 and s1.id=s3.id+1)
+    )
+order by s1.id;
