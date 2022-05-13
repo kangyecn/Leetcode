cf0fe664a2affe2923df86ed8498535abbb308cb
kangyecn <93654227+kangyecn@users.noreply.github.com>
2022-05-13 11:14:44 +1000

    Time: 1533 ms (21.84%), Space: 0B (100.00%) - LeetHub

--- 607-sales-person/607-sales-person.sql
+++ 607-sales-person/607-sales-person.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
-
-select name from salesperson where name not in
-(
-select s.name from salesperson as s
-left join orders as o 
-on s.sales_id = o.sales_id
-left join company as c
-on o.com_id = c.com_id
-where c.name ='Red'
+select name from salesperson
+where name not in 
+    (select s.name
+     from salesperson as s
+     left join orders as o
+     on s.sales_id = o.sales_id
+     left join company as c
+     on o.com_id = c.com_id
+     where c.name = 'red'
     )
