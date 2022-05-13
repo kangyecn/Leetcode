90f0c54992049dc80b87ee19ad68d05c05fd516c
kangyecn <93654227+kangyecn@users.noreply.github.com>
2022-05-13 11:29:43 +1000

    Time: 1447 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 626-exchange-seats/626-exchange-seats.sql
+++ 626-exchange-seats/626-exchange-seats.sql
@@ -1,8 +1,11 @@
 # Write your MySQL query statement below
-
-select (case
-when mod(id,2) = 1 and lead(id) over(order by id) is null then id
-when mod(id,2) = 0 then id-1
-when mod(id,2) = 1 then id+1
-end) as id, student
-from seat order by id;
+select 
+    (case
+     when mod (id,2) = 1 and lead (id) over(order by id) is null then id
+     when mod (id,2) = 0 then id-1
+     when mod (id,2) = 1 then id+1
+     end
+    ) as id,
+    student
+from seat
+order by id;
