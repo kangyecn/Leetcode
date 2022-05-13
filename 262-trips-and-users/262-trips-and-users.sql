d0d9a96584f383f20edbe868993e7345c5139058
kangyecn <93654227+kangyecn@users.noreply.github.com>
2022-05-13 10:45:16 +1000

    Time: 658 ms (18.44%), Space: 0B (100.00%) - LeetHub

--- 262-trips-and-users/262-trips-and-users.sql
+++ 262-trips-and-users/262-trips-and-users.sql
@@ -1,6 +1,25 @@
-select request_at as Day, round(sum(if(status!='completed',1,0))/count(status),2) as 'Cancellation Rate' from
-Trips where Request_at >='2013-10-01' and Request_at <= '2013-10-03'
-and client_id in (select users_id from Users where banned ='No')
-and driver_id in (select users_id from Users where banned = 'No')
+select 
+    request_at as day,
+    round(
+        sum(
+            if(
+                status='completed',0,1
+            )
+        )/count('status'),2
+    ) as 'Cancellation Rate'
+from trips
+where
+    request_at >= '2013-10-01'
+    and request_at <= '2013-10-03'
+    and client_id in 
+        (select users_id from users 
+         where 
+            banned = 'No'
+            and role = 'client')
+    and driver_id in
+        (select users_id from users
+        where 
+            banned = 'No'
+            and role = 'driver')
 group by request_at
 order by request_at
